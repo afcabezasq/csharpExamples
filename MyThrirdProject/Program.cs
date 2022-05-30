4dc7fd062c43ad53f7358e924c7b1a48c2eca6bc
HAEMO\ACABEZAS <acabezas@haemonetics.com>
2022-05-30 16:59:52 -0400

    First explanation

--- MyThrirdProject/Program.cs
+++ MyThrirdProject/Program.cs
@@ -6,7 +6,25 @@ namespace MyThrirdProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello, this is the stsrt of the program, please enter if you have an umbrella: \n");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+
+            if (input == "false")
+            {
+                Console.WriteLine("Oh no I cannot go outside I don't have an umbrella");                
+                
+            }
+            else
+            {
+                Console.WriteLine("Good thing I have an Umbrella so I can go outside while it's raining!");
+                
+            }
+
+            
+
         }
     }
+
 }
